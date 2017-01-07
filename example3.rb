56d21c7f50f25c7715849bcb0f0a064a6e1c0c4e
apiepho <alpiepho@gmail.com>
2017-01-07 09:54:21 -0700

    GC scraping: started building list of games from json data

--- example3.rb
+++ example3.rb
@@ -1,51 +1,90 @@
 require 'mechanize'
 require 'json'
 
-email = 'alpiepho@yahoo.com'
-password = 'jjjzzzkkk'
+# TODO: pass is as params or config file
+email        = 'alpiepho@yahoo.com'
+password     = 'jjjzzzkkk'
+
 GC_BASE_URI  = 'https://gc.com'
 GC_LOGIN_URI = GC_BASE_URI + '/login'
-GC_TEAM_MAX  = '100'
-GC_TEAMS_URI = GC_BASE_URI + '/teams?page_number=0&page_size=' + GC_TEAM_MAX
+GC_TEAMS_URI = GC_BASE_URI + '/teams?page_number=0&page_size=1000' # assume no one will follow more than 1000 teams
 
+class Game
+    def initialize(mechanize, game_json)
+    @json = game_json
+# example game_json
+=begin
+{"home_won"=>false,
+ "away_won"=>true,
+ "arrival_ts"=>nil,
+ "playstatus"=>"CPT",
+ "meta"=>{"offline"=>true},
+ "result"=>"L",
+ "home"=>true,
+ "timezone"=>-7.0,
+ "utc_start"=>"2016-03-12T19:00:00",
+ "sport"=>"BASEB",
+ "team_name"=>"Roosevelt",
+ "recap_title"=>
+  "Eaton (CO) pulls away late for 12-4 victory over Roosevelt Rough Riders Varsity ",
+ "state"=>
+  {"status"=>"CPT",
+   "inning"=>10,
+   "side_readable"=>"Top",
+   "away"=>12,
+   "x"=>"st",
+   "w"=>"v",
+   "half"=>0,
+   "home"=>4,
+   "side"=>0},
+ "location"=>"Nelson Field 4",
+ "play_time"=>"2016-03-12T12:00:00",
+ "type"=>"Exhibition",
+ "save_ts"=>1457829771.0,
+ "recipients"=>[],
+ "wl"=>"0-0-0",
+ "other_team_id"=>"56e4c51a57a7013ee2000001",
+ "game_id"=>"56e4c51a57a7013ef9000002",
+ "uses_halves"=>false,
+ "notes"=>"",
+ "recapstatus"=>"CPT",
+ "stream_id"=>"56e428119ddd0a3f1f000001",
+ "minutes_before"=>0,
+ "other_team_name"=>"Eaton"}
+=end
+    end
+    
+    def display
+        puts "    %s"   % @json["game_id"]
+        puts "      %s" % (@json["location"].empty? ? "-" : @json["location"])
+        puts "      %s" % @json["result"]
+    end
+end
 
 class Team
-    #attr_accessor :complex_name
-    #attr_accessor :relative_href
-    #attr_accessor :href
-    #attr_accessor :name
-# possible fields
-#    .parsed_name
-#    .parsed_href
-#    .name
-#    .sport
-#    .season
-#    .year
-#    .total_games
-#    .games
 
+	def initialize(mechanize, relative_href)
 
-	def initialize(mechanize, complex_name, relative_href)
-	    # save given params
-	    @mechanize = mechanize
-		@complex_name = complex_name
-		@relative_href = relative_href
-
-        # parse the params
-		@name = complex_name.gsub("Baseball", "").gsub("Softball", "").gsub("Fan", "").gsub("·", "").strip
-		@href = GC_BASE_URI + @relative_href
-		parts = relative_href.split("/")
-		parts = parts[2].split("-")
-		@season = parts[0].strip.capitalize
-		@year = parts[1].strip
+        # parse the team relative href
+		@href = GC_BASE_URI + relative_href
+		# using http://rubular.com/ to figure out regex for something like
+		# "/t/spring-2016/roosevelt-rough-riders-varsity-56dfa90020277d0024b46bbb"
+		match = relative_href.match(/\/t\/([a-z]+)-([0-9]+)\/([a-z0-9-]+)-([a-z0-9]+\z)/).to_a
+		# should produce:
+		# match[0] = /t/spring-2016/roosevelt-rough-riders-varsity-56dfa90020277d0024b46bbb
+		# match[1] = spring
+		# match[2] = 2016
+		# match[3] = roosevelt-rough-riders-varsity
+		# match[4] = 56dfa90020277d0024b46bbb
+		@name   = match[3].gsub("-", " ").split.map{|i| i.capitalize}.join(' ')
+		@season = match[1].capitalize
+		@year   = match[2]
+		@guid   = match[4]
 		
-		# get info from the team page
+		# get the team page
 		page = mechanize.get(@href)
-        #puts "======================="
-        #puts @name
-        #puts page.body
         
-        # on team page, get city and sport that is shown under team name
+        # from team page, get city and sport that is shown under team name
         temp   = page.parser.css('.pll h2').text.gsub("\n", "").strip
         temp   = page.parser.css('.pll h3').text.gsub("\n", "").strip if temp.length == 0 # tournaments use h3
         parts  = temp.split("·")
@@ -55,7 +94,7 @@ class Team
         @sport = parts[1].strip if parts.length == 2  # normal display for teams
         @sport = parts[0].strip if parts.length == 1  # tournaments don't show city
 
-        # parse game summary from team page
+        # from team page, parse game summary
         # mechanize/nokogiri do NOT run javascript, but GC passes game summary data as a JSON string
         # TODO: try watir or selenium to get html AFTER javascript???
         temp = page.body                                           # get body as a string
@@ -67,33 +106,36 @@ class Team
         json_encoded = json_encoded.gsub("\'),", "")               # remove traikling cruft
         json_decoded = JSON.parse json_encoded                     # conver to a hash
 
-        # compute W-L-T record from game json game summary
+        # from json game summary, compute W-L-T record and build list of Games
         @wins   = 0
         @losses = 0
         @ties   = 0
-        json_decoded.each do |game|
-			@wins   += 1 if game["result"] === "W"
-			@losses += 1 if game["result"] === "L"
-			@ties   += 1 if game["result"] === "T"
+        @games = []
+        json_decoded.each do |game_json|
+			@wins   += 1 if game_json["result"] === "W"
+			@losses += 1 if game_json["result"] === "L"
+			@ties   += 1 if game_json["result"] === "T"
+			@games << Game.new(mechanize, game_json)
         end        
  
 	end
 	
-	def display(full = false)
+	def display
 		puts "%s"         % @name
-		puts "  %s"       % @complex_name   if full
-		puts "  %s"       % @relative_href  if full
 		puts "  %s"       % @href
+		puts "  %s"       % @guid
 		puts "  %s"       % @city
 		puts "  %s"       % @sport
 		puts "  %s-%s"    % [@season, @year]
 		puts "  %d-%d-%d" % [@wins, @losses, @ties]
+		@games.each do |game|
+		    game.display
+		end
 	end
 end
 
 def dump_teams(teams)
 	teams.each do |team|
-	  #team.display(full=true)
 	  team.display
 	end
 	puts "total: %d" % teams.length
@@ -112,9 +154,8 @@ form.submit
 
 # get teams
 page = mechanize.get(GC_TEAMS_URI)
-#pp page.body
 
-# using href that start with /t/ and include 
+# team links have href that start with /t/ and include 
 team_links = []
 page.links.each do |link|
     team_links << link if link.href.to_s.start_with?('/t/') and link.text.include?(' Fan ')
@@ -122,8 +163,8 @@ end
 
 teams = []
 team_links.each do |link|
-    next if not (link.text.include?("Rough") or link.text.include?("Xplosion"))
-    team = Team.new(mechanize, link.text, link.href)
+    next if not (link.text.include?("Rough") or link.text.include?("Xplosion")) # debug, limit teams
+    team = Team.new(mechanize, link.href)
     teams << team
 end
 
