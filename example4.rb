f6f041eecbb30c44a77286c41387dd154618f416
piephoa <piephoa@c02pt1esg8wp.ftc.avagotech.net>
2017-01-10 15:16:26 -0700

    GC: add more details to games (scraping json)

--- example4.rb
+++ example4.rb
@@ -17,59 +17,22 @@ $total_teams = 0
 $total_games = 0
 
 class Game
+    attr_accessor :score_us, :score_them
+
 	def initialize(browser, options, game_json)
 		$total_games += 1
 		@json = game_json
-# example game_json, one element from team json data
-=begin
-{"home_won"=>false,
- "away_won"=>true,
- "arrival_ts"=>nil,
- "playstatus"=>"CPT",
- "meta"=>{"offline"=>true},
- "result"=>"L",
- "home"=>true,
- "timezone"=>-7.0,
- "utc_start"=>"2016-03-12T19:00:00",
- "sport"=>"BASEB",
- "team_name"=>"Roosevelt",
- "recap_title"=>
-  "Eaton (CO) pulls away late for 12-4 victory over Roosevelt Rough Riders Varsity ",
- "state"=>
-  {"status"=>"CPT",
-   "inning"=>10,
-   "side_readable"=>"Top",
-   "away"=>12,
-   "x"=>"st",
-   "w"=>"v",
-   "half"=>0,
-   "home"=>4,
-   "side"=>0},
- "location"=>"Nelson Field 4",
- "play_time"=>"2016-03-12T12:00:00",
- "type"=>"Exhibition",
- "save_ts"=>1457829771.0,
- "recipients"=>[],
- "wl"=>"0-0-0",
- "other_team_id"=>"56e4c51a57a7013ee2000001",
- "game_id"=>"56e4c51a57a7013ef9000002",
- "uses_halves"=>false,
- "notes"=>"",
- "recapstatus"=>"CPT",
- "stream_id"=>"56e428119ddd0a3f1f000001",
- "minutes_before"=>0,
- "other_team_name"=>"Eaton"}
-=end
-	uri = GC_BASE_URI + "/game-" + @json["game_id"] + "/plays"
-	browser.goto(uri)
+		uri = GC_BASE_URI + "/game-" + @json["game_id"] + "/plays"
+		browser.goto(uri)
       
-	# get body as a string
-	count = 0
-	begin
-		sleep 1
-		count += 1
+		# get body as a string, waiting for javascript to finish populating
 		temp = browser.html
-	end until temp.include?(" inning_1_half_0 \"") or count > 30
+    	seconds = 0
+    	while not temp.include?(" inning_1_half_0 \"") and seconds < 30 do
+        	sleep(1)
+			seconds += 1
+        	temp = browser.html
+		end
 =begin
 	json_encoded = temp.match(/initialize\(\$\.parseJSON.*$/)              # get the JSON data with GC game data
 	json_encoded = json_encoded.to_s                                       # convert MatchData to a string
@@ -83,13 +46,74 @@ class Game
 end
     
 	def display
-		puts "    %s"   % @json["game_id"]
-		puts "      %s" % (@json["location"].empty? ? "-" : @json["location"])
-		puts "      %s" % @json["result"]
+=begin
+		# example game_json, one element from team json data
+		{"home_won"=>false,
+		 "away_won"=>true,
+		 "arrival_ts"=>nil,
+		 "playstatus"=>"CPT",
+		 "meta"=>{"offline"=>true},
+		 "result"=>"L",
+		 "home"=>true,
+		 "timezone"=>-7.0,
+		 "utc_start"=>"2016-03-12T19:00:00",
+		 "sport"=>"BASEB",
+		 "team_name"=>"Roosevelt",
+		 "recap_title"=>
+		  "Eaton (CO) pulls away late for 12-4 victory over Roosevelt Rough Riders Varsity ",
+		 "state"=>
+		  {"status"=>"CPT",
+		   "inning"=>10,
+		   "side_readable"=>"Top",
+		   "away"=>12,
+		   "x"=>"st",
+		   "w"=>"v",
+		   "half"=>0,
+		   "home"=>4,
+		   "side"=>0},
+		 "location"=>"Nelson Field 4",
+		 "play_time"=>"2016-03-12T12:00:00",
+		 "type"=>"Exhibition",
+		 "save_ts"=>1457829771.0,
+		 "recipients"=>[],
+		 "wl"=>"0-0-0",
+		 "other_team_id"=>"56e4c51a57a7013ee2000001",
+		 "game_id"=>"56e4c51a57a7013ef9000002",
+		 "uses_halves"=>false,
+		 "notes"=>"",
+		 "recapstatus"=>"CPT",
+		 "stream_id"=>"56e428119ddd0a3f1f000001",
+		 "minutes_before"=>0,
+		 "other_team_name"=>"Eaton"}
+=end
+        at_vs        = (@json["home"] === "home" ? "vs" : "at")
+		win_lose_tie = "win"   if (@json["result"] === "W")
+		win_lose_tie = "loss"  if (@json["result"] === "L")
+		win_lose_tie = "tie"   if (@json["result"] === "T")
+        score_home   = @json["state"]["home"].to_i
+        score_away   = @json["state"]["away"].to_i
+        @score_us    = (@json["home"] ? score_home : score_away)
+        @score_them  = (@json["home"] ? score_away : score_home)
+
+		puts "    %s %s" % [at_vs, @json["other_team_name"]]
+		puts "      %s" % @json["utc_start"]
+		#puts "      game_id:  %s" % @json["game_id"]
+		puts "      location: %s" % (@json["location"].empty? ? "-" : @json["location"])
+		puts "      us:       %s" % @score_us
+		puts "      them:     %s" % @score_them
+		puts "      result:   %s" % win_lose_tie
+		puts "      recap:    %s" % @json["recap_title"]
 	end
 end
 
 class Team
+
+	def supported_game(options, game_json)
+		result = true
+     	result = false if not options.games.nil? and @games.length >= options.games.to_i
+        result = false if game_json.nil? or game_json["game_id"].nil?
+        result
+	end
 
 	def initialize(browser, options, href)
 		$total_teams += 1
@@ -150,16 +174,18 @@ class Team
 		# from json game summary,  build list of Games
 		@games = []
 		json_decoded.each do |game_json|
-			next if not options.games.nil? and @games.length >= options.games.to_i
-			@games << Game.new(browser, options, game_json) if not game_json.nil? and not game_json["game_id"].nil?
-		end        
+			next if not supported_game(options, game_json)
+			@games << Game.new(browser, options, game_json)
+		end
+		# games are listed in newest first
+        @games = @games.reverse
  
 	end
    
 	def display
 		puts "%s"         % @name
 		puts "  %s"       % @href
-		puts "  %s"       % @guid
+		#puts "  %s"       % @guid
 		puts "  %s"       % @city
 		puts "  %s"       % @sport
 		puts "  %s-%s"    % [@season, @year]
