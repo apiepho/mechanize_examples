9354c010035ceb23c1604ddd312d34f2bd22e7aa
piephoa <piephoa@c02pt1esg8wp.ftc.avagotech.net>
2017-01-10 14:01:01 -0700

    GC: some clean up and added command line params

--- example4.rb
+++ example4.rb
@@ -1,11 +1,9 @@
 require 'json'
 require 'nokogiri'
+require 'optparse'
+require 'ostruct'
 require 'pp'
 require 'watir'
-
-# TODO: pass is as params or config file
-email        = 'alpiepho@yahoo.com'
-password     = 'jjjzzzkkk'
 
 GC_BASE_URI  = 'https://gc.com'
 GC_LOGIN_URI = GC_BASE_URI + '/login'
@@ -14,13 +12,14 @@ GC_LOGIN_URI = GC_BASE_URI + '/login'
 # NOTE: since we are parsong json instead of html, don't need to set page_size
 GC_TEAMS_URI = GC_BASE_URI + '/teams'
 
+# debug/status
 $total_teams = 0
 $total_games = 0
 
 class Game
-    def initialize(browser, game_json)
-$total_games += 1
-    	@json = game_json
+	def initialize(browser, options, game_json)
+		$total_games += 1
+		@json = game_json
 # example game_json, one element from team json data
 =begin
 {"home_won"=>false,
@@ -61,41 +60,41 @@ $total_games += 1
  "minutes_before"=>0,
  "other_team_name"=>"Eaton"}
 =end
-        uri = GC_BASE_URI + "/game-" + @json["game_id"] + "/plays"
-		browser.goto(uri)
-		
-        # get body as a string
-        count = 0
-        begin
-    		sleep 1
-    		count += 1
-            temp = browser.html
-        end until temp.include?(" inning_1_half_0 \"") or count > 30
+	uri = GC_BASE_URI + "/game-" + @json["game_id"] + "/plays"
+	browser.goto(uri)
+      
+	# get body as a string
+	count = 0
+	begin
+		sleep 1
+		count += 1
+		temp = browser.html
+	end until temp.include?(" inning_1_half_0 \"") or count > 30
 =begin
-        json_encoded = temp.match(/initialize\(\$\.parseJSON.*$/)              # get the JSON data with GC game data
-        json_encoded = json_encoded.to_s                                       # convert MatchData to a string
-        json_encoded = json_encoded.gsub("\\u0022", "\"")                      # convert unicode quote
-        json_encoded = json_encoded.gsub("\\u002D", "-")                       # convert unicode hyphen
-        json_encoded = json_encoded.gsub("initialize\(\$\.parseJSON\(\"", "")  # remove leading cruft
-        json_encoded = json_encoded.gsub("\"\), \$\(\'body\'\)\);", "")        # remove trailing cruft
-        json_decoded = JSON.parse json_encoded                                 # conver to a hash
-        pp json_decoded
+	json_encoded = temp.match(/initialize\(\$\.parseJSON.*$/)              # get the JSON data with GC game data
+	json_encoded = json_encoded.to_s                                       # convert MatchData to a string
+	json_encoded = json_encoded.gsub("\\u0022", "\"")                      # convert unicode quote
+	json_encoded = json_encoded.gsub("\\u002D", "-")                       # convert unicode hyphen
+	json_encoded = json_encoded.gsub("initialize\(\$\.parseJSON\(\"", "")  # remove leading cruft
+	json_encoded = json_encoded.gsub("\"\), \$\(\'body\'\)\);", "")        # remove trailing cruft
+	json_decoded = JSON.parse json_encoded                                 # conver to a hash
+	pp json_decoded
 =end
-    end
+end
     
-    def display
-        puts "    %s"   % @json["game_id"]
-        puts "      %s" % (@json["location"].empty? ? "-" : @json["location"])
-        puts "      %s" % @json["result"]
-    end
+	def display
+		puts "    %s"   % @json["game_id"]
+		puts "      %s" % (@json["location"].empty? ? "-" : @json["location"])
+		puts "      %s" % @json["result"]
+	end
 end
 
 class Team
 
-	def initialize(browser, href)
-$total_teams += 1
+	def initialize(browser, options, href)
+		$total_teams += 1
 
-        # parse the team href
+		# parse the team href
 		# using http://rubular.com/ to figure out regex for something like
 		# "/t/spring-2016/roosevelt-rough-riders-varsity-56dfa90020277d0024b46bbb"
 		match = href.match(/\/t\/([a-z]+)-([0-9]+)\/([a-z0-9-]+)-([a-z0-9]+\z)/).to_a
@@ -110,54 +109,53 @@ $total_teams += 1
 		@season = match[1].capitalize
 		@year   = match[2]
 		@guid   = match[4]
-		
+      
 		# get the team page
 		browser.goto(@href)
         
-        # from team page, get city and sport that is shown under team name
+		# from team page, get city and sport that is shown under team name
 		doc = Nokogiri::HTML(browser.html)
-        temp   = doc.css('.pll h2').text.gsub("\n", "").strip
-        temp   = doc.css('.pll h3').text.gsub("\n", "").strip if temp.length == 0 # tournaments use h3
-        parts  = temp.split("·")
-        @city  = "-"
-        @sport = "-"
-        @city  = parts[0].strip if parts.length == 2  # normal display for teams
-        @sport = parts[1].strip if parts.length == 2  # normal display for teams
-        @sport = parts[0].strip if parts.length == 1  # tournaments don't show city
+		temp   = doc.css('.pll h2').text.gsub("\n", "").strip
+		temp   = doc.css('.pll h3').text.gsub("\n", "").strip if temp.length == 0 # tournaments use h3
+		parts  = temp.split("·")
+		@city  = "-"
+		@sport = "-"
+		@city  = parts[0].strip if parts.length == 2  # normal display for teams
+		@sport = parts[1].strip if parts.length == 2  # normal display for teams
+		@sport = parts[0].strip if parts.length == 1  # tournaments don't show city
 
-        # from team page, parse game summary
-        # mechanize/nokogiri do NOT run javascript, but GC passes game summary data as a JSON string
-        # watir can get html AFTER javascript, but full set of team json if always give, so this
-        # way we don't need special uri for teams.
-        temp = browser.html                                        # get body as a string
-        json_encoded = temp.match(/ \$\.parseJSON.*$/)             # get the JSON data with GC game data
-        json_encoded = json_encoded.to_s                           # convert MatchData to a string
-        json_encoded = json_encoded.gsub("\\u0022", "\"")          # convert unicode quote
-        json_encoded = json_encoded.gsub("\\u002D", "-")           # convert unicode hyphen
-        json_encoded = json_encoded.gsub(" \$\.parseJSON(\'", "")  # remove leading cruft
-        json_encoded = json_encoded.gsub("\'),", "")               # remove trailing cruft
-        json_decoded = JSON.parse json_encoded                     # conver to a hash
+		# from team page, parse game summary
+		# mechanize/nokogiri do NOT run javascript, but GC passes game summary data as a JSON string
+		# watir can get html AFTER javascript, but full set of team json if always give, so this
+		# way we don't need special uri for teams.
+		temp = browser.html                                        # get body as a string
+		json_encoded = temp.match(/ \$\.parseJSON.*$/)             # get the JSON data with GC game data
+		json_encoded = json_encoded.to_s                           # convert MatchData to a string
+		json_encoded = json_encoded.gsub("\\u0022", "\"")          # convert unicode quote
+		json_encoded = json_encoded.gsub("\\u002D", "-")           # convert unicode hyphen
+		json_encoded = json_encoded.gsub(" \$\.parseJSON(\'", "")  # remove leading cruft
+		json_encoded = json_encoded.gsub("\'),", "")               # remove trailing cruft
+		json_decoded = JSON.parse json_encoded                     # conver to a hash
 
-        # from json game summary, compute W-L-T record and build list of Games
-        @wins   = 0
-        @losses = 0
-        @ties   = 0
-        json_decoded.each do |game_json|
+		# from json game summary, compute W-L-T record and build list of Games
+		@wins   = 0
+		@losses = 0
+		@ties   = 0
+		json_decoded.each do |game_json|
 			@wins   += 1 if game_json["result"] === "W"
 			@losses += 1 if game_json["result"] === "L"
 			@ties   += 1 if game_json["result"] === "T"
-        end        
+		end        
 
-        # from json game summary,  build list of Games
-        @games = []
-        json_decoded.each do |game_json|
-			@games << Game.new(browser, game_json) if not game_json.nil? and not game_json["game_id"].nil?
-#debug-limit games
-#break
-        end        
+		# from json game summary,  build list of Games
+		@games = []
+		json_decoded.each do |game_json|
+			next if not options.games.nil? and @games.length >= options.games.to_i
+			@games << Game.new(browser, options, game_json) if not game_json.nil? and not game_json["game_id"].nil?
+		end        
  
 	end
-	
+   
 	def display
 		puts "%s"         % @name
 		puts "  %s"       % @href
@@ -167,19 +165,58 @@ $total_teams += 1
 		puts "  %s-%s"    % [@season, @year]
 		puts "  %d-%d-%d" % [@wins, @losses, @ties]
 		@games.each do |game|
-		    game.display
+			game.display
 		end
 	end
 end
 
+def supported_team(year, season, teams, href)
+	result = true
+	if result and not year.nil?
+		result = false
+		result = true if href.include?(year + "/")
+	end
+	if result and not season.nil?
+		result = false
+		result = true if href.include?("/" + season.downcase)
+	end
+	if result and not teams.nil?
+		result = false
+		parts = teams.split(",")
+		parts.each do |part|
+			result = true if href.include?("/" + part.strip)
+		end
+	end
+	result
+end
+
 def dump_teams(teams)
 	teams.each do |team|
-	  team.display
+		team.display
 	end
+	puts $total_teams
+	puts $total_games
 end
 
 
+# parse command line options
+options = OpenStruct.new
+parser = OptionParser.new do |opt|
+	opt.on('-e', '--email    <email>',                   '(required) login email for GameChanger')                         { |o| options.email    = o }
+	opt.on('-p', '--password <password>',                '(required) login password for GameChanger')                      { |o| options.password = o }
+	opt.on('-l', '--list',                               '(optional) list team urls')                                      { |o| options.list     = true }
+	opt.on('-y', '--year <YYYY>',                        '(optional) specific year')                                       { |o| options.year     = o }
+	opt.on('-s', '--season <spring|summer|fall|winter>', '(optional) specfic season')                                      { |o| options.season   = o }
+	opt.on('-t', '--teams <teams>',                      '(optional) list of sub strings to limit teams (separated by ,)') { |o| options.teams    = o }
+	opt.on('-g', '--games <games>',                      '(debug)    limit games')                                         { |o| options.games    = o }
+end
+parser.parse!
 
+if options.email.nil? or options.password.nil?
+	puts "Error: must provide email and password."
+	puts parser.help
+	exit
+end
 
 # get a new instance of Watir class (NOTE: need chromedriver in path, 
 # get chromedriver from https://sites.google.com/a/chromium.org/chromedriver/downloads)
@@ -187,8 +224,8 @@ browser = Watir::Browser.new
 
 # login
 browser.goto(GC_LOGIN_URI)
-browser.text_field(:id,'email').set(email)
-browser.text_field(:id,'login_password').set(password)
+browser.text_field(:id,'email').set(options.email)
+browser.text_field(:id,'login_password').set(options.password)
 browser.form(:id,'frm_login').submit
 
 # go to teams page
@@ -197,20 +234,20 @@ browser.goto(GC_TEAMS_URI)
 # team links have href that includes /t/
 team_links = []
 browser.links.to_a.each do |link|
-    team_links << link.href if link.href.to_s.include?('/t/')
+	team_links << link.href if link.href.to_s.include?('/t/')
 end
 team_links = team_links.uniq
 
 teams = []
 team_links.each do |link|
-#debug-limit teams
-#next if not (link.include?("rough"))
-    team = Team.new(browser, link)
-    teams << team
+	next if not supported_team(options.year, options.season, options.teams, link)
+	if options.list
+		puts link
+	else
+		team = Team.new(browser, options, link)
+		teams << team
+   end
 end
 
-dump_teams(teams)
-puts $total_teams
-puts $total_games
-
+dump_teams(teams) if not options.list
 
